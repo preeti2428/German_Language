3f79948220c6fe842aa234637d9bd812d6cbbcc0
java.146 <preetisinghthakur07@gmail.com>
2026-08-20 17:22:32 +0530

    fix: allow all cors origins for vercel preview links

--- jaiman-backend/index.ts
+++ jaiman-backend/index.ts
@@ -17,10 +17,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: ['https://german-language-chi.vercel.app', 'http://localhost:3000'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // Mount Routes
